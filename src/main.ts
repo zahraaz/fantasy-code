a2e321c0d0e129dafcf60231a95de2d20eda9f5d
zahraaz <zahraamahmood222@gmail.com>
2024-02-03 10:13:15 +0300

    adding validation to the post req. body throw the useage of pipes, class-validator and introducting dtos

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
